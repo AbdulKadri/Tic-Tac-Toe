cba5cd67f1410790685845e70a6722e67c9f1a90
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2022-11-02 23:39:28 -0600

    change info to object

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const gameboardModule = (() => {
 
-    const setUpGame = (info) => {
-        info = {}
-        info.gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-        info.player1 = "X"
-        info.player2 = "O"
-        info.round = 0
-        info.currentPlayer = "X"
-        info.gameOver = false
+    const popUp = document.getElementById('start-Button')
+    popUp.addEventListener('click', (e) => {
+        e.preventDefault()
 
-        return(info)
-    }
+        document.getElementById('start-Popup').classList.add('hide')
+        
+        let info = {
+            gameboard: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+            player1: "X",
+            player2: "O",
+            round: 0,
+            currentPlayer: "X",
+            gameOver: false
+        }
+        startGame(info)
+    })
+
+    // const setUpGame = (info) => {
+    //     info = {}
+    //     info.gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    //     info.player1 = "X"
+    //     info.player2 = "O"
+    //     info.round = 0
+    //     info.currentPlayer = "X"
+    //     info.gameOver = false
+
+    //     return(info)
+    // }
 
     const addEventListenersToGameBoard = (info) => {
         const sections = document.querySelectorAll(".sections")
@@ -22,7 +39,7 @@ const gameboardModule = (() => {
     }
 
     const startGame = (info) => {
-        setUpGame(info)
+        // setUpGame(info)
 
         addEventListenersToGameBoard(info)
 
@@ -30,8 +47,18 @@ const gameboardModule = (() => {
     }
 
     const useMove = (section, info) => {
+        if (info.gameOver === true || info.round > 8) {
+            return
+        }
+
+        if (info.gameboard[section.id] === "X" || info.gameboard[section.id] === "O") {
+            return
+        }
+
+        info.gameboard[section.id] = info.currentPlayer
+        section.textContent = info.currentPlayer
+        info.round++
+
         console.log(section, info)
     }
-
-    startGame()
 })();
